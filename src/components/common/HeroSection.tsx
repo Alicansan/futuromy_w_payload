1b84314b8a1d4193013144c0e5166987e10f41f6
Alicansan <alicansan2606@gmail.com>
2024-12-05 15:04:26 +0300

    translation

--- src/components/common/HeroSection.tsx
+++ src/components/common/HeroSection.tsx
@@ -7,7 +7,7 @@ const HeroSection = () => {
   const t = useTranslations('HeroSection');
 
   return (
-    <div className="container mx-auto flex max-w-7xl flex-col items-center py-12 text-center">
+    <div className="mx-auto flex max-w-7xl flex-col items-center py-12 text-center">
       <h1 className="mx-auto text-2xl font-black sm:text-3xl md:w-[50vw] md:text-5xl lg:text-7xl">
         {t('title.before')}
         <span className="text-primary">{t('title.highlight1')}</span>
@@ -29,7 +29,7 @@ const HeroSection = () => {
         <p className="text-base font-semibold md:text-lg">{t('buttonText')}</p>
       </Button>
       <p className="p-3 text-sm text-muted-foreground"> {t('noCommitment')} </p>
-      <p className="font-dancingScript text-5xl text-popover">
+      <p className="font-dancingScript text-5xl text-primary">
         {' '}
         {t('launch')}{' '}
       </p>
